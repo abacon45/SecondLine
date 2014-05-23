e9d45242225dc9d80314a80cdab2e4755bea3595
abacon45 <abacon45@aol.com>
2014-05-23 11:11:47 -0500

    Added functionality for options and encyclopedia buttons

--- Second Line of Defense/StartMenu.java
+++ Second Line of Defense/StartMenu.java
@@ -12,12 +12,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 
 public class StartMenu extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtSecondLineOf;
 
 	/**
 	 * Launch the application.
@@ -46,15 +47,39 @@ public class StartMenu extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Encyclopedia");
+		JLabel lblSecondLineOf = new JLabel("Second Line of Defense");
+		lblSecondLineOf.setFont(new Font("Helvetica Neue", Font.PLAIN, 26));
+		lblSecondLineOf.setForeground(Color.WHITE);
+		lblSecondLineOf.setBounds(110, 0, 294, 61);
+		contentPane.add(lblSecondLineOf);
+		
+		final JButton btnNewButton = new JButton("Encyclopedia");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (arg0.getSource() == btnNewButton) {
+					dispose();
+					EncyclopediaFrame e = new EncyclopediaFrame();
+					e.setVisible(true);
+				}
+			}
+		});
 		btnNewButton.setBounds(201, 299, 117, 29);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Options");
+		final JButton btnNewButton_1 = new JButton("Options");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (arg0.getSource() == btnNewButton_1) {
+					dispose();
+					OptionsFrame o = new OptionsFrame();
+					o.setVisible(true);
+				}
+			}
+		});
 		btnNewButton_1.setBounds(201, 258, 117, 29);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Load");
+		final JButton btnNewButton_2 = new JButton("Load");
 		btnNewButton_2.setBounds(201, 217, 117, 29);
 		contentPane.add(btnNewButton_2);
 		
@@ -70,12 +95,6 @@ public class StartMenu extends JFrame {
 		});
 		btnNewButton_3.setBounds(201, 177, 117, 29);
 		contentPane.add(btnNewButton_3);
-		
-		txtSecondLineOf = new JTextField();
-		txtSecondLineOf.setText("Second Line of Defense");
-		txtSecondLineOf.setBounds(176, 62, 161, 28);
-		contentPane.add(txtSecondLineOf);
-		txtSecondLineOf.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setBounds(0, 0, 500, 372);
