27a68b36eac40edc0ae0e022706d9966e6152926
Shaun Meyer <shaun.meyer@banno.com>
2014-05-30 11:14:57 -0500

    Basic fighting between bacteria and cytotoxic cells

--- Second Line of Defense/Units/IUnit.java
+++ Second Line of Defense/Units/IUnit.java
@@ -4,4 +4,7 @@ public interface IUnit {
   public int ID();
   public boolean equals(Object object);
   public void setLocation(int x, int y);
+//  public void takeDamage(int damage);
+//  public int dealDamage();
+//  public int checkHealth();
 }
